13fb1529c878e661aa1222d86bc4a37c287c636e
Katerina <katrona2000@gmail.com>
2025-06-09 18:32:43 +0300

    set-up commit

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,8 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <div className="p-4">
+      <h1 className="text-3xl font-bold text-blue-600">Broket is live!</h1>
+    </div>  
   </StrictMode>,
 )
